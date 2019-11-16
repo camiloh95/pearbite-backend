fc299736d224c6ea0f13f3b2e35410a584dd369a
Sala K 402 <salak402@itmsalas.edu.co>
2019-11-16 07:18:41 -0500

    db config and iteration schema

--- src/controllers/test.controller.js
+++
@@ -1,5 +0,0 @@
-const testController = (req, res, next) => {
-    res.status(200).send({ mensaje: 'Esta webada esta funcionando desde aws'});
-};
-
-module.exports = testController;
--- src/routes/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('express').Router();
-
-app.get('/test', require('../controllers/test.controller'));
-
-module.exports = app;
